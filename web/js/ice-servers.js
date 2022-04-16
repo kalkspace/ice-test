7284588b31e5e58222bda4ffe9c25db41aae2ee8
Marcus Weiner <marcus.weiner@gmail.com>
2022-04-16 13:01:49 +0200

    Format JS

--- web/js/ice-servers.js
+++ web/js/ice-servers.js
@@ -1,131 +1,137 @@
-window.iceServers = []
+window.iceServers = [];
 
-let iceListDiv = $('#ice-list')
+let iceListDiv = $("#ice-list");
 
 try {
-  let localStorage = window.localStorage
-  let storedIceServers = localStorage.getItem('iceServers')
+  let localStorage = window.localStorage;
+  let storedIceServers = localStorage.getItem("iceServers");
   if (storedIceServers) {
-    iceServers = JSON.parse(storedIceServers)
+    iceServers = JSON.parse(storedIceServers);
   }
 } catch (e) {
-  console.error(e)
+  console.error(e);
 }
 
-renderIceServers()
+renderIceServers();
 
 function renderIceServers() {
-  iceListDiv.empty()
+  iceListDiv.empty();
 
-  let hasActiveElement = false
+  let hasActiveElement = false;
   window.iceServers.forEach((element, index) => {
-    renderElement(element, index)
+    renderElement(element, index);
     if (element.isActive) {
-      hasActiveElement = true
+      hasActiveElement = true;
     }
-  })
+  });
 
-  startButton[0].disabled = !hasActiveElement
+  startButton[0].disabled = !hasActiveElement;
 
   if (!window.iceServers.length) {
-    let template = `<div class="row"><div class="col-12">No ICE servers, please add at least one below</div></div>`
-    let e = document.createElement('div')
-    e.innerHTML = template
-    iceListDiv.append(e.childNodes[0])
+    let template = `<div class="row"><div class="col-12">No ICE servers, please add at least one below</div></div>`;
+    let e = document.createElement("div");
+    e.innerHTML = template;
+    iceListDiv.append(e.childNodes[0]);
   }
 }
 
 function renderElement(element, index) {
-  let template = `<div class="row" data-id="${index}"><div class="col-9">`
+  let template = `<div class="row" data-id="${index}"><div class="col-9">`;
 
-  if (element.type === 'stun') {
-    template += `URL: ${element.url}`
+  if (element.type === "stun") {
+    template += `URL: ${element.url}`;
   } else {
-    template += `URL: ${element.url} Username: ${element.username} Credential: ${element.credential}`
+    template += `URL: ${element.url} Username: ${element.username} Credential: ${element.credential}`;
   }
 
   template += `</div>
-  <div class="col-1 text-center"><input type="checkbox" class="active-checkbox" ${element.isActive ? 'checked' : ''}></div>
+  <div class="col-1 text-center"><input type="checkbox" class="active-checkbox" ${
+    element.isActive ? "checked" : ""
+  }></div>
   <div class="col-2 text-right">
     <a href="#" class="btn btn-outline-secondary delete-server">
         <i class="fas fa-trash-alt"></i>
     </a>
-</div>`
+</div>`;
 
-  let e = document.createElement('div')
-  e.innerHTML = template
-  iceListDiv.append(e.childNodes[0])
+  let e = document.createElement("div");
+  e.innerHTML = template;
+  iceListDiv.append(e.childNodes[0]);
 }
 
-$('#stun-form').submit((event) => {
-  event.preventDefault()
+$("#stun-form").submit((event) => {
+  event.preventDefault();
 
   let server = {
-    type: 'stun',
+    type: "stun",
     isActive: true,
-    url: $('#add-new-stun').val()
-  }
+    url: $("#add-new-stun").val(),
+  };
 
-  addServer(server)
-  event.currentTarget.reset()
-  renderIceServers()
-})
+  addServer(server);
+  event.currentTarget.reset();
+  renderIceServers();
+});
 
-
-$('#turn-form').submit((event) => {
-  event.preventDefault()
+$("#turn-form").submit((event) => {
+  event.preventDefault();
 
   let server = {
-    type: 'turn',
+    type: "turn",
     isActive: true,
-    url: $('#add-new-turn').val(),
-    username: $('#add-new-turn-username').val(),
-    credential: $('#add-new-turn-credential').val()
-  }
+    url: $("#add-new-turn").val(),
+    username: $("#add-new-turn-username").val(),
+    credential: $("#add-new-turn-credential").val(),
+  };
 
-  addServer(server)
-  event.currentTarget.reset()
-  renderIceServers()
-})
+  addServer(server);
+  event.currentTarget.reset();
+  renderIceServers();
+});
 
 function addServer(server) {
   try {
-    let localStorage = window.localStorage
-    let storedIceServers = localStorage.getItem('iceServers')
+    let localStorage = window.localStorage;
+    let storedIceServers = localStorage.getItem("iceServers");
     if (storedIceServers) {
-      iceServers = JSON.parse(storedIceServers)
+      iceServers = JSON.parse(storedIceServers);
     }
-    iceServers.push(server)
+    iceServers.push(server);
 
-    localStorage.setItem('iceServers', JSON.stringify(iceServers))
+    localStorage.setItem("iceServers", JSON.stringify(iceServers));
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
-function removeServer (index) {
-  iceServers.splice(index, 1)
-  localStorage.setItem('iceServers', JSON.stringify(iceServers))
+function removeServer(index) {
+  iceServers.splice(index, 1);
+  localStorage.setItem("iceServers", JSON.stringify(iceServers));
 }
 
-function changeActiveServer (index, isActive) {
-  iceServers[index].isActive = !!isActive
-  localStorage.setItem('iceServers', JSON.stringify(iceServers))
+function changeActiveServer(index, isActive) {
+  iceServers[index].isActive = !!isActive;
+  localStorage.setItem("iceServers", JSON.stringify(iceServers));
 }
 
+iceListDiv.on("click", ".delete-server", (event) => {
+  event.preventDefault();
+  let id = parseInt(
+    event.currentTarget.parentElement.parentElement.dataset.id,
+    10
+  );
 
-iceListDiv.on('click', '.delete-server', (event) => {
-  event.preventDefault()
-  let id = parseInt(event.currentTarget.parentElement.parentElement.dataset.id, 10)
+  removeServer(id);
+  renderIceServers();
+});
 
-  removeServer(id)
-  renderIceServers()
-})
+iceListDiv.on("change", ".active-checkbox", (event) => {
+  event.preventDefault();
+  let id = parseInt(
+    event.currentTarget.parentElement.parentElement.dataset.id,
+    10
+  );
 
-iceListDiv.on('change', '.active-checkbox', (event) => {
-  event.preventDefault()
-  let id = parseInt(event.currentTarget.parentElement.parentElement.dataset.id, 10)
-
-  changeActiveServer(id, event.currentTarget.checked)
-  renderIceServers()
-})
+  changeActiveServer(id, event.currentTarget.checked);
+  renderIceServers();
+});
